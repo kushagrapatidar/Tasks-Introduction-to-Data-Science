2d7706f3dfc32f2f25927c23f41452f964553618
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-10-26 11:22:34 +0530

    main

--- line_graphs.py
+++ line_graphs.py
@@ -1,8 +1,6 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
-xpoints = np.array([0, 6])
-ypoints = np.array([0, 250])
-
-plt.plot(xpoints, ypoints)
+x=[1,2,3,4,5,6,7,8]
+y=[0.1,0.2,0.5,0.6,0.8,0.8,0.9,1.3]
+plt.plot(x,y)
 plt.show()
