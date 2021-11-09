03dbb7d52e4c46eb9494d29f5d5a6dbbf194f863
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-11-09 17:00:18 +0530

    main

--- Task_10/Task_10.b.py
+++ Task_10/Task_10.b.py
@@ -1,8 +1,7 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-happiness_ds = pd.read_csv('2015.csv')
+happiness_ds = pd.read_csv('worldhappiness2015.csv')
 happiness_ds = pd.DataFrame(happiness_ds)
 
 mean_happiness = {}
@@ -14,4 +13,4 @@ for r in regions:
     region_group_mean = region_group['Happiness Score'].mean()
     mean_happiness[r] = region_group_mean
 
-print(mean_happiness)
+[print(r,':',mean_happiness[r]) for r in regions]
