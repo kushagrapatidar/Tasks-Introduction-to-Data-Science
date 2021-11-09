1a2c048ce2c811ead66f476c728496bcb92f9853
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-11-09 16:51:54 +0530

    main

--- Task_10/Task_10.a.py
+++ Task_10/Task_10.a.py
@@ -1,12 +1,12 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-happiness_ds = pd.read_csv('2015.csv')
+happiness_ds = pd.read_csv('worldhappiness2015.csv')
 happiness_ds = pd.DataFrame(happiness_ds)
 
 # print(happiness_ds['Country'])
 
 # happiness_ds = happiness_ds[:5]
+
 happiness_ds['Happiness Score'].plot(kind='bar', title='Happiness Scores', ylim=(0,10))
 plt.show()
